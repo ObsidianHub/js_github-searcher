48122c6e89a7665e5c23a98214c4b0baccd02f75
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-12 18:06:12 +0300

    write getUser method in Github class

--- js/Github.js
+++ js/Github.js
@@ -3,4 +3,16 @@ class Github {
     this.client_id = "3c40bdf6b44d98324f14";
     this.client_secret = "12dcd64f203af8b77d0cbbc0fa3a528da63ca80e";
   }
+
+  // Get user by name
+  getUser(name) {
+    return new Promise((resolve, reject) => {
+      fetch(
+        `https://api.github.com/users/${name}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      )
+        .then((res) => res.json())
+        .then((user) => resolve(user))
+        .catch((err) => reject(err));
+    });
+  }
 }
