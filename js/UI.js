70f92a4a4bbdb43576921a3ef4af00fa8c33c408
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-12 18:09:03 +0300

    init our UI class, created constructor

---
+++ js/UI.js
@@ -0,0 +1,7 @@
+class UI {
+  constructor() {
+    this.profile = document.getElementById("profile");
+    this.search_container = document.querySelector(".searchContainer");
+    this.loader = document.querySelector(".loader");
+  }
+}
