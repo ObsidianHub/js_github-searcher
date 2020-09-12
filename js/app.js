423f458d9647bb12c4bccfd59e340155c653f50f
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-12 18:19:00 +0300

    end write app.js

--- js/app.js
+++ js/app.js
@@ -11,4 +11,28 @@ const searchInput = document.getElementById("searchUser");
 searchInput.addEventListener("keyup", (e) => {
   // get input text
   const userText = e.target.value;
+
+  if (userText !== "") {
+    ui.showLoader();
+
+    github
+      .getUserAsync(userText)
+      .then((info) => {
+        if (info.userData.message === "Not Found") {
+          // Show alert
+          ui.showAlert(`User: ${userText} not found`, "alert alert-danger");
+          // Clear profile
+          ui.clearProfile();
+          ui.hideLoader();
+        } else {
+          ui.showProfile(info.userData);
+          ui.showRepos(info.reposData);
+          ui.clearAlert();
+        }
+      })
+      .then(() => ui.hideLoader());
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
 });
